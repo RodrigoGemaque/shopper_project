c8f5b5add0447c5e0d2758ccf164423c4d763c3d
rodrigo <ro.gemaque16@gmail.com>
2021-07-12 10:38:24 -0300

    reset project

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_190245) do
+ActiveRecord::Schema.define(version: 2021_07_10_200210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
-    t.string "title", null: false
-    t.bigint "product_id", null: false
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["product_id"], name: "index_categories_on_product_id"
   end
 
   create_table "order_items", force: :cascade do |t|
@@ -42,6 +40,15 @@ ActiveRecord::Schema.define(version: 2021_07_10_190245) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "product_categories", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_product_categories_on_category_id"
+    t.index ["product_id"], name: "index_product_categories_on_product_id"
+  end
+
   create_table "product_variants", force: :cascade do |t|
     t.string "title", null: false
     t.decimal "price", precision: 15, scale: 2, null: false
@@ -59,8 +66,9 @@ ActiveRecord::Schema.define(version: 2021_07_10_190245) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "categories", "products"
   add_foreign_key "order_items", "orders"
   add_foreign_key "order_items", "products"
+  add_foreign_key "product_categories", "categories"
+  add_foreign_key "product_categories", "products"
   add_foreign_key "product_variants", "products"
 end
